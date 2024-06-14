aaf7e52f174aaf93fcc90d5866bab38031678d6a
Banquetox <banquetox@gmail.com>
2024-06-14 09:49:12 -0600

    cors policy

--- PracticarRogusApi/Program.cs
+++ PracticarRogusApi/Program.cs
@@ -6,7 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "cors",
+        policy => {
+            policy.AllowAnyOrigin();
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+        }
+        );
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("cors");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
