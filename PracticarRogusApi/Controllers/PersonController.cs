c479d54102f78ce8f80d8ca53194eea2deac6625
Banquetox <banquetox@gmail.com>
2024-06-14 09:13:50 -0600

    Add project files.

---
+++ PracticarRogusApi/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticarRogusApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetPersonDataLinq(string id)
+        {
+            try
+            {
+                return Ok("response");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetPersonDataSqlCommand(string id)
+        {
+            try
+            {
+                return Ok("response");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
